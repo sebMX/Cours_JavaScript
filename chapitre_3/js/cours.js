7dff2a4cf64506f14aaf4b30355775bea3239c47
sebMX <seb.massonneau@gmail.com>
2018-06-09 23:19:18 +0200

    travail sur if et else en JS

--- chapitre_3/js/cours.js
+++ chapitre_3/js/cours.js
@@ -2,6 +2,24 @@
 //Si ce nombre est positif
 //Afficher un message
 
-var nombre = Number(prompt("Entrez un nombre"));
-if (nombre > 0);
+var nombre = Number(prompt("Entrez un nombre :"));
+if (nombre > 0) {
 	console.log(nombre + " est positif");
+}
+else {
+	console.log(nombre + " est négatif ou nul");
+}
+
+// Imbriquer des conditions
+/*
+var nombre = Number(prompt("Entrez un nombre :"));
+if (nombre > 0) {
+    console.log(nombre + " est positif");
+} else { // nombre <= 0
+    if (nombre < 0) {
+        console.log(nombre + " est négatif");
+    } else { // nombre === 0
+        console.log(nombre + " est nul");
+    }
+}
+*/
