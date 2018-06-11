774907201cd60bb665da163348186336e4f29d61
sebMX <seb.massonneau@gmail.com>
2018-06-11 20:26:12 +0200

    Fin chapitre 4

--- chapitre_4/js/triangle.js
+++ chapitre_4/js/triangle.js
@@ -1,7 +1,8 @@
 // Exercice : construire et afficher un triangle de 7 lignes
-
+/*
 var compteur = "";
 for (var i = 1; i <= 7; i++) {
 	compteur = compteur + "#";	// OU compteur += "#"
 	console.log(compteur);
 }
+*/
